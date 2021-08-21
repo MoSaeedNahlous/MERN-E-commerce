f1a9a6d451272849dceb1d6ecda56aa0ae93eea1
Mo Saeed Nahlous <jay.haji041@gmail.com>
2021-08-21 10:10:47 +0300

    homescreen and productscreen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
+import React from 'react'
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
 import './index.css';
+import HomeScreen from './screens/HomeScreen';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+
+
 
 const App =()=> {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route exact path='/' component={HomeScreen} />
+          <Route exact path='/product/:id' component={ProductScreen}/>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
